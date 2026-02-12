9b6f98c6d4adb57a6bc62d29b545f4f89ebdcc92
Abhishek Patel <147897001+GitAbhii@users.noreply.github.com>
2026-02-13 05:19:08 +0530

    Create BinarySearch.java

---
+++ Day8/BinarySearch.java
@@ -0,0 +1,29 @@
+public class BinarySearchExample {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50, 60, 70};
+        int key = 50;
+
+        int low = 0;
+        int high = arr.length - 1;
+        int position = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == key) {
+                position = mid;
+                break;
+            } else if (arr[mid] < key) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        if (position != -1)
+            System.out.println("Element found at index " + position);
+        else
+            System.out.println("Element not found");
+    }
+}
