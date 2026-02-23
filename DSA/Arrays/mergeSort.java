2e2c88b2263bec0988da9a0e75495833c8b546ad
Abhishek Patel <147897001+GitAbhii@users.noreply.github.com>
2026-02-23 23:30:46 +0530

    Create mergeSort.java

---
+++ DSA/Arrays/mergeSort.java
@@ -0,0 +1,26 @@
+public static int[] merge(int[] arr1, int[] arr2) {
+    int n = arr1.length;
+    int m = arr2.length;
+
+    int[] merged = new int[n + m];
+
+    int i = 0, j = 0, k = 0;
+
+    while (i < n && j < m) {
+        if (arr1[i] <= arr2[j]) {
+            merged[k++] = arr1[i++];
+        } else {
+            merged[k++] = arr2[j++];
+        }
+    }
+
+    while (i < n) {
+        merged[k++] = arr1[i++];
+    }
+
+    while (j < m) {
+        merged[k++] = arr2[j++];
+    }
+
+    return merged;
+}
