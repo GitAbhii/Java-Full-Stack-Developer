32a3446275ab80ba12ce6f4f1f3342b819f4d89e
Abhishek Patel <147897001+GitAbhii@users.noreply.github.com>
2026-02-23 23:35:49 +0530

    Create Palindrome.java

---
+++ DSA/Recursion/Palindrome.java
@@ -0,0 +1,7 @@
+public class Palindrome {
+    public static boolean isPalindrome(String s, int l, int r){
+        if(l >= r) return true;
+        if(s.charAt(l) != s.charAt(r)) return false;
+        return isPalindrome(s, l+1, r-1);
+    }
+}
