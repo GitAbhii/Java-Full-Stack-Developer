783960848327389ee4a01848f958e52f8760820f
Abhishek Patel <147897001+GitAbhii@users.noreply.github.com>
2026-03-07 23:47:39 +0530

    Create RotateString.java

---
+++ DSA/Strings/RotateString.java
@@ -0,0 +1,12 @@
+// Leetdoce 796. Rotate String
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        String s1 = s+s;
+        if(s.length()==goal.length()){
+            return s1.contains(goal);
+        }else{
+            return false;
+        }
+
+    }
+}
